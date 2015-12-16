24111b293ec520b6f7aa0e01a11083b5c2cd0a8b
TexusDark <texusdark.offcial@gmail.com>
2015-12-16 13:55:41 +0100

    Remove other fish.
    False ids and meta for salmon, pufferfish, clownfish.

--- src/pocketmine/item/Fish.php
+++ src/pocketmine/item/Fish.php
@@ -30,13 +30,6 @@ namespace pocketmine\item;
 class Fish extends Item implements Food{
 	public function __construct($meta = 0, $count = 1){
 		parent::__construct(self::RAW_FISH, $meta, $count, "Raw Fish");
-		if($meta === 1){
-			$this->name = "Raw Salmon";
-		}elseif($meta === 2){
-			$this->name = "Clownfish";
-		}elseif($meta === 3){
-			$this->name = "Pufferfish";
-		}
 	}
 
         public function isEatable() {
